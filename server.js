bb96e5538de2b0c0e09de7bff5293f3cc7e6e7f3
nndeto <86444679+nndeto@users.noreply.github.com>
2021-09-23 17:11:09 -0400

    added routes to server page for login, signup

--- server.js
+++ server.js
@@ -20,6 +20,55 @@ app.use(session({ secret: process.env.SESSION_SECRET }));
 //Controllers
 app.use('/listings', listingController);
 
+
+//user routes
+//signup post route
+app.post('/signup', (req, res) => {
+    console.log(req.body);
+    db.User.create(req.body, (err, createdUser) => {
+      if (err) console.log(err);
+      console.log(createdUser);
+      req.session.currentUser = createdUser;
+        res.json({
+            message: "You're signed up!"
+        });
+    });
+  })
+
+  
+//login route to check if user exists
+app.post("/login", (req, res) => {
+    console.log(req.body);
+    db.User.findOne({ username: req.body.username }, (err, foundUser) => {
+      if (err) return console.log(err);
+      if (!foundUser) {
+        return res.json({
+            message: "Username  not found"
+        });
+      }
+      if (req.body.password !== foundUser.password) {
+        return res.json({
+            message: "Wrong password"
+        });
+      }
+      req.session.currentUser = foundUser;
+      console.log(req.session);
+      res.json({
+          message: "You are logged in"
+      });
+    });
+  });
+
+//logout route to destroy session
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    console.log("you're logged out")
+  })
+  
+
+
+
+
 //checking turned on port
 app.listen(port, () => {
     console.log(`Finding you a roommate at ${port}.`)
